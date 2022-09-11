12dbebd1e6961d4335afc7f1ea095f454bb255d6
Agathe Lurquin <agathe.lurquin@essca.eu>
2022-09-11 20:54:25 +0200

    stimulus fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,2 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
-
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,10 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
+
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
